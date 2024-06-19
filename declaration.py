17dc1fc07137ce19d277614964f289e42d531c21
Prabhakar-Naik <prabhakarnauik225@gmail.com>
2024-06-19 16:19:52 +0530

    changes on user

---
+++ declaration.py
@@ -0,0 +1,45 @@
+# naming formate must follow mean
+# string str
+name = "prabhakar"
+print(name)
+
+# number int
+count = 20
+print(count)
+
+# decimal float
+salary = 20000.4
+print(salary)
+
+# list of data
+list = [1,2,3,4,5,6]
+print(list)
+
+# tuble of data
+tuple = (10,20,30,40,50,60)
+print(tuple)
+
+# dictionary stores key value pair
+dictionary = {"key":"value","name":"prabha","score":200}
+print(dictionary)
+
+print(dictionary.get("key"))
+print(dictionary.values())
+print(dictionary.keys())
+print(dictionary.items())
+print(dictionary.setdefault("name"))
+print(dictionary)
+
+# set of data
+set = set()
+set.add("prabha")
+set.add(3)
+set.add(13)
+set.add("harsha")
+set.add(23)
+set.add(33)
+print(set)
+
+
+# set not accepted list and tuple inside
+
