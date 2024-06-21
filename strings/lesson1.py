806b8c766d134c97b5a023edc2884396cfa0a8ad
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-06-21 12:00:16 +0530

    strings

---
+++ strings/lesson1.py
@@ -0,0 +1,22 @@
+name = "prabhakar"
+
+print(type(name))
+print(name)
+
+characters = [one for one in name]
+print(type(characters))
+print(characters)
+
+print(name,"length is: ",len(name))
+print("characters length is:",len(characters))
+
+name_upper = name.upper()
+print(name_upper)
+print(name_upper.lower())
+
+name_split = name.split('a')
+
+print(name_split)
+
+
+
---
+++ strings/reverse_string.py
@@ -0,0 +1,18 @@
+
+text = "text"
+
+print(text)
+
+# reverse strings
+
+def reverse_string(data):
+    return data[::-1]
+
+reverse = reverse_string(text)
+print(reverse)
+
+print(reverse_string(text))
+
+
+
+
