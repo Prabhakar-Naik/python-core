4eb39f798a1805332e6bba8a32472e1e0515758c
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-06-24 10:35:09 +0530

    numeric functions completed

---
+++ numerics/math_module_functions.py
@@ -0,0 +1,72 @@
+import math
+
+# Returns the ceiling of x, the smallest integer greater than or equal to x.
+print(math.ceil(4.5))  # Output: 5
+
+# Returns the floor of x, the largest integer less than or equal to x.
+print(math.floor(4.8))  # Output: 4
+
+# return factorial of given number
+print(math.factorial(5))  # Output: 120
+
+# Returns the greatest common divisor of a and b.
+print(math.gcd(48, 18))  # Output: 6
+
+# Returns the square root of x
+print(math.sqrt(16))  # Output: 4.0
+
+# Returns e raised to the power of x.
+print(math.exp(2))  # Output: 7.38905609893065
+
+
+# Returns the logarithm of x to the given base. If the base is not specified, returns the natural logarithm of x.
+print(math.log(8, 2))  # Output: 3.0
+
+# Returns the base-10 logarithm of x.
+print(math.log10(100))  # Output: 2.0
+
+#  Returns the base-2 logarithm of x.
+print(math.log2(8))  # Output: 3.0
+
+
+# Returns the sine of x radians.
+print(math.sin(math.pi / 2))  # Output: 1.0
+
+# Returns the cosine of x radians.
+print(math.cos(0))  # Output: 1.0
+
+# Returns the tangent of x radians.
+print(math.tan(math.pi / 4))  # Output: 1.0
+
+
+# Returns the arc sine of x, in radians.
+print(math.asin(1))  # Output: 1.5707963267948966
+
+# Returns the arc cosine of x, in radians.
+print(math.acos(1))  # Output: 0.0
+
+
+# Returns the arc tangent of x, in radians.
+print(math.atan(1))  # Output: 0.7853981633974483
+
+
+# Converts angle x from radians to degrees.
+print(math.degrees(math.pi))  # Output: 180.0
+
+# Converts angle x from degrees to radians.
+print(math.radians(180))  # Output: 3.141592653589793
+
+
+# Returns the Euclidean norm, sqrt(x*x + y*y).
+print(math.hypot(3, 4))  # Output: 5.0
+
+# Additional Libraries for Numerical Computation
+"""
+
+NumPy: Provides support for large, multi-dimensional arrays and matrices, along with a collection of mathematical functions to operate on these arrays.
+SciPy: Builds on NumPy and provides additional functionality for scientific and technical computing.
+Pandas: Provides data structures and data analysis tools.
+SymPy: A library for symbolic mathematics.
+
+"""
+
--- numerics/numeric_functions.py
+++ numerics/numeric_functions.py
@@ -1,24 +1,43 @@
-result = []
-for i in range(1,10):
-    result.append(i*2)
-    
-    
-
-print(result)
+# result = []
+# for i in range(1,10):
+#     result.append(i*2)
 
 
-product = [num * 2 for num in range(10)]
-print(product)
+# print(result)
 
+# product = [num * 2 for num in range(10)]
+# print(product)
 
-def calculate_circle_area(radius):
-    return 3.14 * radius * radius
+# def calculate_circle_area(radius):
+#     return 3.14 * radius * radius
 
-def calculate_square_area(side):
-    return side * side
+# def calculate_square_area(side):
+#     return side * side
 
-section = 'A'
+# section = 'A'
 
-my_class = '10 th standard'
+# my_class = '10 th standard'
+
+# return absolute value
+print(abs(-5))  # Output: 5
+
+# 7 devided by 3, return uple containing the quotient and remainder
+print(divmod(7, 3))  # Output: (2, 1)
+
+# return max value from given parameterized values
+print("max value of 1,3,2: =>",max(1, 3, 2))  # Output: 3
+
+# return min value from given parameterized values
+print("min value of 1,3,2: =>",min(1, 3, 2))  # Output: 1
+
+# return 2 power of 3 mean 2*2*2 = 8
+print(pow(2, 3))  # Output: 8
+
+# return second parameter count of digits after decimal value
+print(round(5.678347687, 2))  # Output: 5.68
+
+# return sum of given numbers in list 
+print(sum([1, 2, 3]))  # Output: 6
+
 
 
