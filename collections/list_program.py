05abb0cc35f0f05a0a9a797ba552ae7322246ab1
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-07-11 10:23:25 +0530

    collection dir added

---
+++ collections/list_program.py
@@ -0,0 +1,21 @@
+fruits = ["apple","banana","grapes",23, 50, "pine-apple","orange"]
+
+print(fruits)
+print(fruits[0])
+print(fruits[-1])
+print(fruits[len(fruits)-1])
+
+print(fruits[0:4])
+print(fruits[:2])
+print(fruits[::-1])
+
+text = "what-is-going-on"
+print(text)
+words = text.split("-")
+print(words)
+
+print(words[0])
+print(words[0:4])
+print(words[0:2])
+print(words[::-1])
+
